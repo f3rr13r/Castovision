e6cb96bdd210d9fb7b7656d17df21e051f679141
Harry <harry@paperclip.co>
2019-04-02 14:16:24 +0100

    feedVC stuff

--- Castovision/Controllers/Main App/Pages/FeedVC.swift
+++ Castovision/Controllers/Main App/Pages/FeedVC.swift
@@ -15,37 +15,20 @@ class FeedVC: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        configureNavigationBar()
-        collectionView.backgroundColor = .clear
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
-        let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 20.0, right: 20.0)
-        layout.itemSize = CGSize(width: screenWidth - (horizontalPadding * 2), height: 180.0)
-        layout.minimumLineSpacing = 12.0
-        self.collectionView!.collectionViewLayout = layout
+        self.configureNavigationBar(withSearchBar: true)
+        self.configureCollectionView()
     }
     
-    func configureNavigationBar() {
-        /*-- logo --*/
-        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30.0, height: 30.0))
-        logoImageView.contentMode = .scaleAspectFit
-        logoImageView.image = #imageLiteral(resourceName: "logo-padded")
-        self.navigationItem.titleView = logoImageView
+    func configureCollectionView() {
+        collectionView.backgroundColor = .clear
         
-        /*-- search bar --*/
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchBar.sizeToFit()
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search.."
-        self.navigationItem.searchController = searchController
-        self.navigationItem.hidesSearchBarWhenScrolling = false
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 20.0, right: 20.0)
+        layout.itemSize = CGSize(width: screenWidth - (horizontalPadding * 2), height: 220.0)
+        layout.minimumLineSpacing = 12.0
+        self.collectionView!.collectionViewLayout = layout
+        
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
 
     // MARK: UICollectionViewDataSource
@@ -56,7 +39,8 @@ class FeedVC: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.backgroundColor = UIColor.orange
+        cell.backgroundColor = UIColor.init(red: 211/255, green: 211/255, blue: 211/255, alpha: 1.0)
+        cell.layer.cornerRadius = 6.0
         return cell
     }
 }
--- Castovision/Extensions/UIViewController.swift
+++ Castovision/Extensions/UIViewController.swift
@@ -12,5 +12,21 @@ extension UIViewController {
     
     func configureNavigationBar(withSearchBar needsSearchBar: Bool) {
         
+        /*-- logo (required) --*/
+        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30.0, height: 30.0))
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.image = #imageLiteral(resourceName: "logo-padded")
+        self.navigationItem.titleView = logoImageView
+        
+        /*-- search bar (optional) --*/
+        if needsSearchBar {
+            let searchController = UISearchController(searchResultsController: nil)
+            searchController.searchBar.sizeToFit()
+            searchController.delegate = self as? UISearchControllerDelegate
+            searchController.obscuresBackgroundDuringPresentation = false
+            searchController.searchBar.placeholder = "Search my self-tapes..."
+            self.navigationItem.searchController = searchController
+            self.navigationItem.hidesSearchBarWhenScrolling = true
+        }
     }
 }
