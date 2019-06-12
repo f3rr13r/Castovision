bb64b97d61e2f0e0e04ab8a3608e9c56ffc30feb
Harry <harry@paperclip.co>
2019-06-12 15:58:00 +0100

    updating project name and also image

--- Castovision/Controllers/Main App/Pages/Settings/Pages/UpdateProfileNameVC.swift
+++ Castovision/Controllers/Main App/Pages/Settings/Pages/UpdateProfileNameVC.swift
@@ -26,19 +26,38 @@ class UpdateProfileNameVC: UIViewController {
         return label
     }()
     
+    let projectNameInput = CustomInputView(inputType: .name)
+    
     let currentAccountName = UserService.instance.currentUser.name
+    var newAccountName = UserService.instance.currentUser.name {
+        didSet {
+            if let newValue = self.newAccountName,
+               newValue.count > 0,
+                newValue != self.currentAccountName {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+            } else {
+                self.navigationItem.rightBarButtonItem?.isEnabled = false
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         lockDeviceVertically()
         self.configureNavigationBar(withTitle: "Update Profile Name", withSearchBar: false)
         addNavigationRightButton()
+        handleChildDelegates()
         anchorChildViews()
     }
     
     func addNavigationRightButton() {
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(updateChanges))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(updateChanges))
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.red
         self.navigationItem.rightBarButtonItem?.isEnabled = false
+    }
+    
+    func handleChildDelegates() {
+        projectNameInput.delegate = self
     }
     
     func anchorChildViews() {
@@ -47,9 +66,34 @@ class UpdateProfileNameVC: UIViewController {
         
         self.view.addSubview(instructionLabel)
         instructionLabel.anchor(withTopAnchor: self.view.safeAreaLayoutGuide.topAnchor, leadingAnchor: self.view.safeAreaLayoutGuide.leadingAnchor, bottomAnchor: nil, trailingAnchor: self.view.safeAreaLayoutGuide.trailingAnchor, centreXAnchor: nil, centreYAnchor: nil, widthAnchor: nil, heightAnchor: nil, padding: .init(top: 12.0, left: horizontalPadding, bottom: 0.0, right: -horizontalPadding))
+        
+        self.view.addSubview(projectNameInput)
+        projectNameInput.input.text = newAccountName
+        projectNameInput.anchor(withTopAnchor: instructionLabel.bottomAnchor, leadingAnchor: self.view.safeAreaLayoutGuide.leadingAnchor, bottomAnchor: nil, trailingAnchor: self.view.safeAreaLayoutGuide.trailingAnchor, centreXAnchor: nil, centreYAnchor: nil, widthAnchor: nil, heightAnchor: nil, padding: .init(top: 24.0, left: horizontalPadding, bottom: 0.0, right: -horizontalPadding))
     }
     
     @objc func updateChanges() {
-        /*-- update it here --*/
+        SharedModalService.instance.showCustomOverlayModal(withMessage: "Updating Account")
+        UserService.instance.updateUserData(withName: "profileName", andValue: self.newAccountName) {
+            (updatedSuccessfully) in
+            SharedModalService.instance.hideCustomOverlayModal()
+            if updatedSuccessfully {
+                self.navigationController?.popViewController(animated: true)
+            } else {
+                let errorMessageConfig = CustomErrorMessageConfig(title: "Something went wrong", body: "We were unable to update your professional name on account. Please try again")
+                SharedModalService.instance.showErrorMessageModal(withErrorMessageConfig: errorMessageConfig)
+            }
+        }
+    }
+}
+
+// custom input delegate methods
+extension UpdateProfileNameVC: CustomInputViewDelegate {
+    func inputValueDidChange(inputType: CustomInputType, inputValue: String) {
+        self.newAccountName = inputValue
+    }
+    
+    func inputClearButtonPressed(inputType: CustomInputType) {
+        self.newAccountName = ""
     }
 }
